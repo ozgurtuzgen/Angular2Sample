12a0afabbd5bf5657435b0cab2d8172ba8152006
ozgurtuzgen <ozgurtuzgen@hotmail.com>
2015-12-29 08:22:33 +0200

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by ozgur.tuzgen on 28.12.2015.
+ */
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1> My second App</h1>'
+})
+
+export class AppComponent{}
